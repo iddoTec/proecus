bf257ef4fefff5e0e12934cff72a520a467dcb5f
xBlackEaglex <roberto.smusic.1@gmail.com>
2024-02-28 09:46:58 -0600

    se cargo la plantilla para el proyecto

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','./node_modules/preline/dist/*.js',],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('preline/plugin'),
+	],
+	darkMode: 'class',
 }
